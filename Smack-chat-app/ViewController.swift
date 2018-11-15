ef719ec65ebf5d44635edb3d595e0f0a326e2367
Charles Davis <tgcd5@me.com>
2018-11-15 10:15:08 -0500

    Initial Commit

---
+++ Smack-chat-app/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack-chat-app
+//
+//  Created by gdm on 11/15/18.
+//  Copyright © 2018 gdm. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
