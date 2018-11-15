c17a26471f898587f2e4f09b7e28bbf08ace0823
gdm <gdm.mbp@gdmMBP.local>
2018-11-15 18:58:00 -0500

    Add LoginVC

---
+++ Smack-chat-app/Controller/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  Smack-chat-app
+//
+//  Created by gdm on 11/15/18.
+//  Copyright © 2018 gdm. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ Smack-chat-app/Utilities/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Smack-chat-app
+//
+//  Created by gdm on 11/15/18.
+//  Copyright © 2018 gdm. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+
+let TO_LOGIN = "toLogin"
