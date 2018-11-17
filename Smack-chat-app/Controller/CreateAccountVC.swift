c7297ff88bca987151d598ebd33b2e2c30eb8521
gdm <gdm.mbp@gdmMBP.local>
2018-11-16 20:49:01 -0500

    CreateAccount functionality

--- Smack-chat-app/Controller/CreateAccountVC.swift
+++ Smack-chat-app/Controller/CreateAccountVC.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class CreateAccountVC: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var userNameText: UITextField!
+    @IBOutlet weak var emailText: UITextField!
+    @IBOutlet weak var passwordText: UITextField!
+    @IBOutlet weak var userImage: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,4 +28,21 @@ class CreateAccountVC: UIViewController {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
     
+    @IBAction func createAccountPressed(_ sender: Any) {
+        // comma here denotes "where"
+        guard let email = emailText.text , emailText.text != "" else { return }
+        guard let pass = passwordText.text , passwordText.text != "" else { return }
+        
+        AuthService.instance.registerUser(email: email, password: pass) { (success) in
+            if success {
+                print("registered user")
+            }
+        }
+    }
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBGColorPressed(_ sender: Any) {
+    }
 }
---
+++ Smack-chat-app/View/RoundedButton.swift
@@ -0,0 +1,32 @@
+//
+//  RoundedButton.swift
+//  Smack-chat-app
+//
+//  Created by gdm on 11/16/18.
+//  Copyright © 2018 gdm. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    
+    override func awakeFromNib() {
+        self.setUpView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
