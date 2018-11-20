3cd6c165a51e22d39f05430e4842125126b24285
gdm <gdm.mbp@gdmMBP.local>
2018-11-20 16:28:55 -0500

    Add channel data model and channel JSON parsing

---
+++ Smack-chat-app/Model/Channel.swift
@@ -0,0 +1,17 @@
+//
+//  Channel.swift
+//  Smack-chat-app
+//
+//  Created by gdm on 11/20/18.
+//  Copyright © 2018 gdm. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    
+    private(set) public var channelTitle: String!
+    private(set) public var channelDescription: String!
+    private(set) public var id: String!
+    
+}
