8e2025c6b47908830757cbe5a8b4c519dbd8a781
gdm <gdm.mbp@gdmMBP.local>
2018-11-21 18:40:08 -0500

    Add unread indicator for channel with unread messages

--- Smack-chat-app/View/ChannelCell.swift
+++ Smack-chat-app/View/ChannelCell.swift
@@ -31,6 +31,15 @@ class ChannelCell: UITableViewCell {
     func configureCell(channel: Channel) {
         let title = channel.channelTitle ?? ""
         channelName.text = "#\(title)"
+        channelName.font = UIFont(name: "HelvitcaNeue-Rgular", size: 17)
+        
+        //Unread channel
+        for id in MessageService.instance.unreadChannels {
+            if id == channel.id {
+                channelName.font = UIFont(name: "HelveticaNeue-Bold", size: 22)
+            }
+        }
+        
     }
 
 }
